5a5d771669624159ffcdc3b19cb8604f4df3bad8
tristanGIANDO <83452758+tristanGIANDO@users.noreply.github.com>
2023-07-25 23:26:03 +0200

    wrap methods

--- PHOTO_GALLERY/database/db.py
+++ PHOTO_GALLERY/database/db.py
@@ -31,10 +31,9 @@ class Database(object):
         return True
 
   def create_file(self, file_name):
-    # columns
-    if not self.file_exists(file_name):
+    if not self.table_exists(file_name):
       columns = "("
-      columns += "id INT AUTO_INCREMENT PRIMARY KEY," # auto increment db index
+      columns += "id INT AUTO_INCREMENT PRIMARY KEY," # auto increment db index but does not work, oh but it is for the table, not row
       columns += "name VARCHAR(45),"
       columns += "path VARCHAR(255)"
       columns += ")"
@@ -43,24 +42,40 @@ class Database(object):
     else:
       print(f"{file_name} already exists.")
 
-  def file_exists(self, file_name=None):
+  def table_exists(self, table=None):
     self._cursor.execute("SHOW TABLES")
     for x in self._cursor:
-      if x[0] == file_name:
+      if x[0] == table:
         return True
-      
+    
+  # fileTable
   def add(self, table:str, values:tuple):
     request = f"INSERT INTO {table} (id, name, path) VALUES (%s,%s,%s)"
-
     self._cursor.execute(request, values)
     self._server.commit()
 
+  # fileTable
   def get_rows(self, table:str):
     self._cursor.execute(f"SELECT * FROM {table}")
-
     return self._cursor.fetchall()
+  
+  # fileTable
+  def get(self, table:str, column:str, value:str):
+    request = f"SELECT * FROM {table} WHERE {column} ='{value}'"
+    self._cursor.execute(request)
+    return self._cursor.fetchall()
+  
+  # fileTable
+  def delete(self, table:str, column:str, value:str):
+    request = f"DELETE FROM {table} WHERE {column} = '{value}'"
+    self._cursor.execute(request)
+    self._server.commit()
+
+  def delete_table(self, table:str):
+    request = f"DROP TABLE {table}"
+    self._cursor.execute(request)
 
 if __name__ == "__main__":
   db = Database()
   db.add("file", ("1", "example.jpg", "path/file"))
-  print(db.get_rows("file"))
+  print(db.delete_table("file"))
